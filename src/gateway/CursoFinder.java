cca4ab07794e5227312298f3642533c959c30bd9
jonesmarxs <jonesmarxs@gmail.com>
2015-07-12 16:57:22 -0300

    Signed-off-by: jonesmarxs <jonesmarxs@gmail.com>

--- src/gateway/CursoFinder.java
+++ src/gateway/CursoFinder.java
@@ -1,21 +1,71 @@
 package gateway;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class CursoFinder implements IFinder {
+import persistencia.ConexaoBD;
 
+public class CursoFinder implements IFinder {
+	protected ConexaoBD conexao;
+
+	public CursoFinder(){
+		conexao = new ConexaoBD();
+	}
 	@Override
 	public ArrayList<IGateway> findAll() {
-		ArrayList<IGateway> gCurso = new ArrayList<IGateway>();
+		ArrayList<IGateway> gCursos = new ArrayList<IGateway>();
+		String sql = "SELECT * FROM curso;";
 		
-		gCurso.add(new CursoGateway("Racicley", "CLEY", new DepartamentoGateway("DepRacicley", "DCLEY")));
-		return gCurso;
+		if (conexao.abrirConexao()) {
+			ResultSet rs = conexao.executarSelectQuery(sql);
+			
+			if ( rs != null ) {
+				try {
+					while (rs.next()) {
+						DepartamentoFinder fDept = new DepartamentoFinder();
+						DepartamentoGateway gDept = (DepartamentoGateway) fDept.find(rs.getString("departamento"));
+						
+						gCursos.add( new CursoGateway(rs.getString("nome"), rs.getString("sigla"), gDept) );
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			conexao.fecharConexao();
+		}
+		
+		return gCursos;
 	}
 
 	@Override
 	public IGateway find(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM curso"
+				+ "WHERE sigla='"+ id +"';";
+		IGateway gCurso = null;
+		
+		if (conexao.abrirConexao()) {
+			ResultSet rs = conexao.executarSelectQuery(sql);
+			if ( rs != null ) {
+				try {
+					while (rs.next()) {
+						DepartamentoFinder fDept = new DepartamentoFinder();
+						DepartamentoGateway gDept = (DepartamentoGateway) fDept.find(rs.getString("departamento"));
+						
+						gCurso = new CursoGateway(rs.getString("nome"), rs.getString("sigla"), gDept);
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			conexao.fecharConexao();
+		}
+			
+		return gCurso;
 	}
 
 }
--- src/gateway/TicketFinder.java
+++ src/gateway/TicketFinder.java
@@ -1,19 +1,79 @@
 package gateway;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class TicketFinder implements IFinder {
+import entidades.Consumidor;
+import entidades.Refeicao;
+import persistencia.ConexaoBD;
 
+public class TicketFinder implements IFinder {
+	
+	protected ConexaoBD conexao;
+
+	public TicketFinder(){
+		conexao = new ConexaoBD();
+	}
+	
 	@Override
 	public ArrayList<IGateway> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<IGateway> gTickets = new ArrayList<IGateway>();
+		String sql = "SELECT * FROM ticket;";
+		
+		if (conexao.abrirConexao()) {
+			ResultSet rs = conexao.executarSelectQuery(sql);
+			
+			if ( rs != null ) {
+				try {
+					while (rs.next()) {
+						RefeicaoFinder fRef = new RefeicaoFinder();
+						ConsumidorFinder fCons = new ConsumidorFinder();
+						RefeicaoGateway gRef = (RefeicaoGateway) fRef.find(rs.getString("refeicao"));
+						ConsumidorGateway gCons = (ConsumidorGateway) fCons.find(rs.getString("consumidor"));
+						
+						gTickets.add(new TicketGateway(rs.getInt("id"), rs.getBoolean("pago"), gRef, gCons));
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			conexao.fecharConexao();
+		}
+		
+		return gTickets;
 	}
 
 	@Override
 	public IGateway find(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM ticket"
+				+ "WHERE sigla='"+ id +"';";
+		IGateway gTicket = null;
+		
+		if (conexao.abrirConexao()) {
+			ResultSet rs = conexao.executarSelectQuery(sql);
+			if ( rs != null ) {
+				try {
+					while (rs.next()) {
+						RefeicaoFinder fRef = new RefeicaoFinder();
+						ConsumidorFinder fCons = new ConsumidorFinder();
+						RefeicaoGateway gRef = (RefeicaoGateway) fRef.find(rs.getString("refeicao"));
+						ConsumidorGateway gCons = (ConsumidorGateway) fCons.find(rs.getString("consumidor"));
+						
+						gTicket = new TicketGateway(rs.getInt("id"), rs.getBoolean("pago"), gRef, gCons);
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			conexao.fecharConexao();
+		}
+			
+		return gTicket;
 	}
 
 }
