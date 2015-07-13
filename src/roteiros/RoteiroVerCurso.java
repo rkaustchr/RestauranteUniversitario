fba4a9e4f09428bccb4856acc3b859385f753919
Rafael Kaustchr <rkaustchr@hotmail.com>
2015-07-13 18:44:59 -0300

    Erro com RoteiroVerCurso

--- src/roteiros/RoteiroVerCurso.java
+++ src/roteiros/RoteiroVerCurso.java
@@ -9,22 +9,18 @@ import gateway.CursoGateway;
 public class RoteiroVerCurso {
 	private String id;
 	
-	public RoteiroVerCurso(String id) {
+	public RoteiroVerCurso( String id ) {
 		this.id = id;
 	}
 
 	public Curso executar() throws CursoNotFound {
 		CursoFinder fCurso = new CursoFinder();
 		Curso retorno;
-		
 		CursoGateway gCurso = (CursoGateway) fCurso.find(id);
 		if ( gCurso == null )  throw new CursoNotFound();
-		
 		retorno = new Curso( gCurso.getNome(), gCurso.getSigla(), new Departamento( gCurso.getDepartamento().getNome(), gCurso.getDepartamento().getSigla()) );
 		
 		return retorno;
 	}
-
-
 
 }
