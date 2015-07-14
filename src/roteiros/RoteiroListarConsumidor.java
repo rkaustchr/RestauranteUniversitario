0bc0ff807bc7559e495101b1f4ab58fa769e604d
Rafael Kaustchr <rkaustchr@hotmail.com>
2015-07-14 19:54:09 -0300

    Construindo o ListarConsumidor

--- src/roteiros/RoteiroListarConsumidor.java
+++ src/roteiros/RoteiroListarConsumidor.java
@@ -1,16 +1,35 @@
 package roteiros;
 
 import java.util.ArrayList;
+
+import entidades.Aluno;
 import entidades.Consumidor;
+import entidades.Curso;
+import entidades.Departamento;
+import gateway.AlunoFinder;
+import gateway.AlunoGateway;
 import gateway.ConsumidorGateway;
+import gateway.CursoFinder;
+import gateway.CursoGateway;
+import gateway.DepartamentoGateway;
+import gateway.FuncionarioFinder;
+import gateway.IGateway;
 
 public class RoteiroListarConsumidor  {
 
 	public ArrayList<Consumidor> executar() throws Exception {
-		ConsumidorGateway gConsumidor = new ConsumidorGateway();
+		AlunoFinder fAluno = new AlunoFinder();
+		FuncionarioFinder fFuncionario = new FuncionarioFinder();
 		
-		return gConsumidor.listarConsumidores();
+		ArrayList<Consumidor> retorno = new ArrayList<Consumidor>();
 		
+		for (IGateway gConsumidor : fAluno.findAll()) {
+			CursoGateway gCurso = ((AlunoGateway) gConsumidor).getCurso();
+			retorno.add(new Aluno(((AlunoGateway) gConsumidor).getNome(), ((AlunoGateway) gConsumidor).getMatricula(), dept));
+		}
+		
+		return retorno;		
+	
 	}
 
 }
