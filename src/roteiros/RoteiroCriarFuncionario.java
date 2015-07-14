0efdc87e4f483a992767800c64e68827ca042674
jonesmarxs <jonesmarxs@gmail.com>
2015-07-14 20:58:37 -0300

    Inserido Criar Aluno/CriarFuncionario
    Signed-off-by: jonesmarxs <jonesmarxs@gmail.com>

---
+++ src/roteiros/RoteiroCriarFuncionario.java
@@ -0,0 +1,42 @@
+package roteiros;
+
+import controladores.ccu.exceptions.DepartamentoNotFound;
+import controladores.ccu.exceptions.NomeNotFoundException;
+import controladores.ccu.exceptions.SiglaNotFoundException;
+import entidades.CPF;
+import entidades.Sexo;
+import entidades.Titulo;
+import gateway.DepartamentoFinder;
+import gateway.DepartamentoGateway;
+import gateway.FuncionarioGateway;
+
+
+public class RoteiroCriarFuncionario {
+	private String nome;
+	private int matricula;
+	private int anoIngresso;
+	private Sexo sexo;
+	private Titulo titulo;
+	private CPF cpf;
+	private String siglaDepartamento;
+	
+	public void execute() throws NomeNotFoundException, SiglaNotFoundException, DepartamentoNotFound{
+		DepartamentoFinder fDepartamento = new DepartamentoFinder();
+		DepartamentoGateway gDepartamento = (DepartamentoGateway) fDepartamento.find(this.siglaDepartamento);
+		if( gDepartamento != null ){			
+			if (this.cpf.toString() == ""){
+				throw new SiglaNotFoundException();
+			}else{
+				if (this.nome==""){
+					throw new NomeNotFoundException();
+				}else{
+					FuncionarioGateway gFuncionario = new FuncionarioGateway(this.nome, this.matricula, this.anoIngresso, this.sexo, this.titulo, this.cpf, gDepartamento);
+					gFuncionario.insert();
+				}
+			}
+		}
+		else{
+			throw new DepartamentoNotFound();
+		}
+	}
+}
